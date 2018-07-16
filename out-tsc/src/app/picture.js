a01208f66f405050bcb567de82ef84e1537e8989
FUJII, Ken <ken.fujii@alibaba-inc.com>
2018-07-16 17:17:44 +0900

    Auto-generated commit

--- out-tsc/src/app/picture.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Picture = /** @class */ (function () {
-    function Picture() {
-    }
-    return Picture;
-}());
-exports.Picture = Picture;
-//# sourceMappingURL=picture.js.map
